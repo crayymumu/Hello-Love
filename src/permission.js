e4829eeaf704f7853c60cb22bcaffe5631669b27
ayden_zhu <ayden_zhu@amaxchina.com>
2020-08-25 16:36:37 +0800

    feat: add baidu track

---
+++ src/permission.js
@@ -0,0 +1,10 @@
+import router from './router'
+
+router.beforeEach(async(to, from, next) => {
+  if (to.path) {
+    if (window._hmt) {
+      window._hmt.push(['_trackPageview', '/#' + to.fullPath])
+    }
+  }
+  next()
+})
